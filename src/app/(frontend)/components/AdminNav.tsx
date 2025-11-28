e088b93b7acb329f04efc7cda0cc9633a535b378
SvannaDam <svanna.dam@akqa.com>
2025-11-28 15:10:58 +0100

    admin style

--- src/app/(frontend)/components/AdminNav.tsx
+++ src/app/(frontend)/components/AdminNav.tsx
@@ -1,10 +1,12 @@
 'use client'
 
+import { File } from "lucide-react"
+
 export default function AdminNav() {
 
   return (
-    <a href="/applications" className="text-black text-5xl fixed bottom-24 right-24 z-50">
-      Admin Page
+    <a href="/applications" className="text-white bg-blue rounded-full text-5xl fixed bottom-24 right-24 z-50 shadow-lg">
+      <File size={30} className="m-4" />
     </a>
   )
 }
