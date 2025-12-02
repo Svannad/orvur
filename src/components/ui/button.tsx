6a5b5ee0bee1d4da36690f3c806614581f934b8b
SvannaDam <svanna.dam@akqa.com>
2025-12-02 14:38:52 +0100

    tab accordion style

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,17 +5,17 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  "cursor-pointer inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
   {
     variants: {
       variant: {
         default: "bg-blue text-white hover:bg-blue/90",
         plain:
-          "hover:text-black/90 text-black",
+          "font-bold text-black hover:italic",
         secondary:
           "bg-white text-black hover:bg-white/90",
         outline:
-          "border border-blue",
+          "border border-blue ",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
@@ -24,6 +24,7 @@ const buttonVariants = cva(
         icon: "size-9",
         "icon-sm": "size-8",
         "icon-lg": "size-10",
+        plain: "p-0 h-9",
       },
     },
     defaultVariants: {
