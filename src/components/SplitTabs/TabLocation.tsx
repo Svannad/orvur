5eda15780601907c5207a2e2ceef044086085f94
SvannaDam <svanna.dam@akqa.com>
2025-12-02 14:40:42 +0100

    tab location style

--- src/components/SplitTabs/TabLocation.tsx
+++ src/components/SplitTabs/TabLocation.tsx
@@ -1,10 +1,10 @@
 export default function TabLoctaion() {
   return (
     <section className="flex flex-col gap-6">
-      <div className="flex flex-col gap-2">
-        <h2 className="text-xl font-bold mb-2">Find Us</h2>
+      <div className="flex flex-col gap-2 mb-6">
+        <h2 className="text-xl font-bold mb-2 italic">Find Us</h2>
         <address className="not-italic">
-          <p>Basement of the Bylgjan building</p>
+          <p className="mb-2">Basement of the Bylgjan building</p>
           <p>Mýravegur 12</p>
           <p>Runavík 620</p>
           <p>Faroe Islands</p>
@@ -13,7 +13,7 @@ export default function TabLoctaion() {
 
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1866.5447176893251!2d-6.726118521820478!3d62.10666567607656!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48bc3a8421c08d1d%3A0x1152c2baa35a1f59!2sBylgjan!5e0!3m2!1sen!2sdk!4v1764677663490!5m2!1sen!2sdk"
-        width={600}
+        width={700}
         height={450}
         style={{ border: '0' }}
         allowFullScreen
